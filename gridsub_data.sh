e148535777beb84d98c7e024919e85eb76883c23
abinashpun <ap563313@ohio.edu>
2022-11-08 15:47:39 -0600

    update on nov8-2022

--- gridsub_data.sh
+++ gridsub_data.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#Author: Abinash pun
+#grid submmiting script
 
 dir_macros=$(dirname $(readlink -f $BASH_SOURCE))
-LIFE_TIME=long # short (3h), medium (8h) or long (23h)
+LIFE_TIME=medium # short (3h), medium (8h) or long (23h)
 
 jobname=$1
 do_sub=$2
@@ -19,14 +21,15 @@ if [ $do_sub == 1 ]; then
 	echo "Forget 'source /e906/app/software/script/setup-jobsub-spinquest.sh'?"
 	exit
     fi
+    #work=/pnfs/e1039/scratch/cosmic_reco_dst/$jobname
     work=/pnfs/e1039/persistent/cosmic_recodata/$jobname
-    
 else
     echo "Local mode."
     work=$dir_macros/scratch/$jobname
 fi
 
-#location of the decoded data
+##location of the decoded data
+#data_dir="/pnfs/e1039/tape_backed/decoded_data"
 data_dir="/pnfs/e1039/scratch/cosmic_decoded_dst"
 
 if [ "$resub_file" = "null" ]; then
@@ -39,18 +42,18 @@ if [ "$resub_file" = "null" ]; then
 
     tar -C $dir_macros -czvf $work/input.tar.gz geom.root RecoE1039Data.C
 
-    #declare -a data_path_list=()
-    if [ $dst_mode = 'single' ] ; then
-	data_path_list=( $data_dir/$(printf 'run_%06d_spin.root' $run_name) )
-    else # 'splitting'     
+    ##declare -a data_path_list=()
+    #if [ $dst_mode = 'single' ] ; then
+    #	data_path_list=( $data_dir/$(printf 'run_%06d_spin.root' $run_name) )
+    #else # 'splitting'     
 	data_path_list=( $(find $data_dir -name $(printf 'run_%06d_spill_*_spin.root' $run_name) ) )
 	echo $data_path_list
-    fi
+    #fi
 
 else
-    
+ 
     data_path_list=( $(find $data_dir -name  $resub_file ) )
-    
+   
 fi #resub_file condition
 
 for data_path in ${data_path_list[*]} ; do
@@ -69,18 +72,19 @@ for data_path in ${data_path_list[*]} ; do
     rsync -av $dir_macros/gridrun_data.sh $work/$job_name/gridrun_data.sh
 
     if [ $do_sub == 1 ]; then
-        cmd="jobsub_submit --grid"
-        cmd="$cmd -l '+SingularityImage=\"/cvmfs/singularity.opensciencegrid.org/e1039/e1039-sl7:latest\"'"
-        cmd="$cmd --append_condor_requirements='(TARGET.HAS_SINGULARITY=?=true)'"
-        cmd="$cmd --use_gftp --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC,OFFSITE -e IFDHC_VERSION --expected-lifetime='$LIFE_TIME'"
-	cmd="$cmd --mail_never"
-	cmd="$cmd -L $work/$job_name/log/log.txt"
-	cmd="$cmd -f $work/input.tar.gz"
-	cmd="$cmd -d OUTPUT $work/$job_name/out"
-	cmd="$cmd -f $data_path"
-	cmd="$cmd file://`which $work/$job_name/gridrun_data.sh` $nevents $run_name $data_file"
-	echo "$cmd"
-	$cmd
+        CMD="/e906/app/software/script/jobsub_submit_spinquest.sh"
+        CMD+=" --expected-lifetime='$LIFE_TIME' --memory=2GB"
+	CMD+=" --mail_never"
+        CMD+=" --lines '+FERMIHTC_AutoRelease=True'"
+        CMD+=" --lines '+FERMIHTC_GraceLifetime=7200'" #2 hours of grace lifetime
+        CMD+=" --lines '+FERMIHTC_GraceMemory=1024'" #1GB of grace memory
+	CMD+=" -L $work/$job_name/log/log.txt"
+	CMD+=" -f $work/input.tar.gz"
+	CMD+=" -d OUTPUT $work/$job_name/out"
+	CMD+=" -f $data_path"
+	CMD+=" file://`which $work/$job_name/gridrun_data.sh` $nevents $run_name $data_file"
+	echo "$CMD"
+	$CMD #| tee $work/$job_name/log_jobsub_submit.txt
     else
 	mkdir -p $work/$job_name/input
 	rsync -av $work/input.tar.gz $data_path  $work/$job_name/input
@@ -88,10 +92,10 @@ for data_path in ${data_path_list[*]} ; do
 	$work/$job_name/gridrun_data.sh $nevents $run_num $data_file | tee $work/$job_name/log/log.txt
 	cd -
     fi | tee $dir_macros/single_log_gridsub.txt
-    
+   
     JOBID="$(tail -2 $dir_macros/single_log_gridsub.txt | head -1 | grep -o '\S*@jobsub\S*')"
     echo $JOBID
     echo $job_name
-    echo  "$job_name $JOBID" >>$dir_macros/jobid_info.txt
+    echo "$job_name $JOBID">>$dir_macros/jobid_info.txt
 
 done 2>&1 | tee $dir_macros/log_gridsub.txt
--- re_run_gridjob.sh
+++ re_run_gridjob.sh
@@ -1,85 +1,89 @@
 #!/bin/bash
-#Script for reusbmission of failed grid jobs 
+#Script for reusbmission of failed grid jobs (bad job nodes and mysql server error)
+#Author: Abinash Pun
 
-#variables to use the updated proxy certificate
+
+##variables to use the updated proxy certificate
 export ROLE=Analysis
 export X509_USER_PROXY=/var/tmp/${USER}.${ROLE}.proxy
 
 source /e906/app/software/script/setup-jobsub-spinquest.sh
 
 dir_scripts=$(dirname $(readlink -f $BASH_SOURCE))
-dir_recofile=/pnfs/e1039/persistent/cosmic_recodata
+dir_recofile=/pnfs/e1039/persistent/cosmic_recodata #/pnfs/e1039/scratch/cosmic_reco_dst
 
-#loop over submitted runs after ~1 hour of job submission
-while read -r RunNum N_splits reco_status; do    
-
-    [[ $reco_status -ne 1 ]] && continue
+##loop over submitted runs after ~1 hour of job submission
+while read -r RunNum N_splits reco_status submission_freq; do
     
-    run_dir=($(printf 'run_%06d' $RunNum) )
+    #    [[ $reco_status -ne 1 ]] && continue  #only checks the uncompleted runs
+    #    [[ $submission_freq -gt 5 ]] && continue # Not more than 5 resubmission
+    if [[ $reco_status -eq 1 && $submission_freq -lt 5 ]]; then   
+	run_dir=($(printf 'run_%06d' $RunNum) )
+	N_GOOD_LOG=0
+	
+	##loop over the log files of the submitted runs
+	for i in $dir_recofile/$run_dir/$run_dir*; do
+	    echo $i
 
-    N_GOOD_LOG=0
-    
-    #loop over the log files of the submitted runs
-    for i in $dir_recofile/$run_dir/$run_dir*; do
-	echo $i
+	    resub_file_base=$(basename $i)
+	    echo $resub_file_base 
+	    resub_file=$resub_file_base'.root'
+            echo $resub_file
 
-	resub_file_base=$(basename $i)
-	echo $resub_file_base
-        
-	resub_file=$resub_file_base'.root'
-        echo $resub_file
+	    ##look at the job id corresponding to resub_file_base with no log file and check status and resubmit
+	    if [ ! -e $i/log/log.txt ]; then
 
-	#look at the job id corresponding to resub_file_base with no log file and check status and resubmit
-	if [ ! -e $i/log/log.txt ]; then
+		while read -r job_name job_id; do
 
-	    while read -r job_name job_id; do
+  		    if [ $job_name == $resub_file_base ]; then
+			printf "%s\n" "job ID: $job_id"
+			JOB_STATUS=$(jobsub_q --jobid $job_id | awk -v i=2 -v j=5 'FNR == i {print $j}')
 
-  		if [ $job_name == $resub_file_base ]; then
+			if [ -z "$JOB_STATUS" ]; then 
+		    	    #remove the row with job_id before resubmitting(will be assigned new job_id)
+		    	    sed -i '/$job_id/d' .$dir_scripts/jobid_info.txt
 
-		    printf "%s\n" "job ID: $job_id"
-		    
-		    JOB_STATUS=$(jobsub_q --jobid $job_id | awk 'FNR == 2 {print $5}')
+			    printf "JOB_STATUS is NULL with no log.txt file, Resubmitting .....%s\n"
+			    echo $resub_file
+			    $dir_scripts/gridsub_data.sh $run_dir 1 $RunNum 0 splitting $resub_file
+			    #(( submission_freq++ ))
+			fi 
+		    fi
+		done < $dir_scripts/jobid_info.txt	    
 
-		    #remove the row with job_id before resubmitting(will be assigned new job_id)
-		    sed '/$job_id/d' .$dir_scripts/jobid_info.txt
+            else
 
-		    if [ -z "$JOB_STATUS" ]; then 
-			printf "JOB_STATUS is NULL with no log.txt file, Resubmitting .....%s\n"
-			echo $resub_file
-			$dir_scripts/gridsub_data.sh $run_dir 1 $RunNum 0 splitting $resub_file
-		    fi 
+		job_status=$(tail -1 "$i/log/log.txt" | head -1) #reco_status from root -l {macro} command in gridrun_data.sh	
+		
+		echo $job_status
 
+		if [ "$job_status" = "0" ]; then
+		    (( N_GOOD_LOG++ ))
 		fi
-	    done < $dir_scripts/jobid_info.txt
-	    
-        else
 
-	    job_status=$(tail -1 "$i/log/log.txt" | head -1) #reco_status from root -l {macro} command in gridrun_data.sh	
-	    
-	    echo $job_status
+		if  ! grep -q 'gridrun.sh finished!' $i/log/log.txt || [ "$job_status" != "0" ]; then	
+	
+            	    #remove the log and output files if any   
+		    rm $dir_recofile/$run_dir/$resub_file_base/log/log.txt
+		    rm $dir_recofile/$run_dir/$resub_file_base/out/*.root
 
-            if [ "$job_status" = "0" ]; then
-		(( N_GOOD_LOG++ ))
-	    fi
+		    #resubmit the grid job
+		    echo "Error while running the macro, Resubmitting ...."
+		    echo $resub_file
+		    $dir_scripts/gridsub_data.sh $run_dir 1 $RunNum 0 splitting $resub_file
+		    #(( submission_freq++ ))
+		fi
 
-
-	    if  ! grep -q 'gridrun.sh finished!' $i/log/log.txt || [ "$job_status" != "0" ]; then
-		
-		#resubmit the grid job
-		echo "Error while running the macro, Resubmitting ...."
-		echo $resub_file
-		$dir_scripts/gridsub_data.sh $run_dir 1 $RunNum 0 splitting $resub_file
-		
-	    fi
-
-        fi
-    done	        
+            fi
+	done	        
+	(( submission_freq++ ))
+    fi
     
     if [ $N_splits -eq $N_GOOD_LOG ]; then
 	reco_status=2
     fi
     
-    paste <(echo "$RunNum") <(echo "$N_splits") <(echo "$reco_status")>>$dir_scripts/reco_status_tmp.txt
+    paste <(echo "$RunNum") <(echo "$N_splits") <(echo "$reco_status") <(echo "$submission_freq")>>$dir_scripts/reco_status_tmp.txt
     
 done <$dir_scripts/reco_status.txt
 
